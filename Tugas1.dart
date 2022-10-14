9ae6b894c91d9d5e5ec796a0dd0b113e6b285415
andrianinur <100122081+andrianinur@users.noreply.github.com>
2022-10-14 09:48:47 +0700

    tugas1praktikum

---
+++ Tugas1.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  String Nama = 'Andriani Nurian';
+  int npm = 20002;
+  double ipk = 3.77;
+
+  print(Nama);
+  print(npm);
+  print(ipk);
+}
---
+++ Tugas3.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  List mahasiswa = ["Nama", "Andriani Nurian"];
+  Map<String, dynamic> data = {
+    "NPM": "201063117002",
+    "Semester": "5",
+    "Kelas": "C",
+  };
+  var dataSet = {04, 01, 03};
+
+  print(mahasiswa);
+  print(data);
+  print(dataSet);
+}
